2aff979d5a8ebb255d9130c0dad5c90c1eb28cb6
Leissan Khan <leissan.khan@gmail.com>
2022-03-17 09:08:39 -0400

    commit tree 3617f8e152393e85d1772857bf20831d2db50d67

--- main.js
+++ main.js
@@ -2,9 +2,77 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+FULL_HEART.class='.activated-heart'
+console.log('FULL_HEART')
 
 
+
+//Add the .hidden class to the error modal in the HTML so it does not appear when the page first loads
+//in index .html we added modal class=hidden
+
+//When a user clicks on an empty heart:
+//Invoke mimicServerCall to simulate making a server request
+
+//Every heart has a class of .like-glyph
+//I need to add an event listener to every instance of that class
+
+let heartsCollection=document.getElementsByClassName('like-glyph')
+console.log(heartsCollection)
+
+//turn heartsCollection into an array
+
+
+
+let heartsNodeArray=[...heartsCollection]
+
+let modal =document.getElementById('modal')
+let modalParagraph=document.getElementById('modal-message')
+
+////add event listener to each heart (two ways below)
+//for (let index=0; index<heartsNodeArray.length; index++){
+  //heartsNodeArray[index].addEventListener('click', mimicServerCall)
+//}
+
+//heartNode is anyname we can use here to indicate we need each element from heartNodeArray
+
+//When the "server" returns a failure status:
+//Respond to the error using a .catch(() => {}) block after your .then(() => {}) block.
+//(so after mimicServer is called, we need to add .catch to it)
+
+let callServerAndCatch=()=>{
+  mimicServerCall()
+  .catch(error=>handleError( error))
+}
+
+//remove class from modal
+//Display the error modal by removing the .hidden class
+//Display the server error message in the modal
+//Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+ let handleError=(errorMessage)=>{
+   modal.classList.remove('hidden')
+   modalParagraph.innerText=errorMessage
+   setTimeout(()=> {
+     modal.classList.add('hidden')
+     modalParagraph.innerText=""
+    }, 3000)
+   p.remove()
+}
+
+heartsNodeArray.map(heartNode=> {
+  heartNode.addEventListener('click', callServerAndCatch)
+})
+
+
+
+
+//When the "server" returns a success status:
+//Change the heart to a full heart
+//Add the .activated-heart class to make the heart appear red
+//When a user clicks on a full heart:
+//Change the heart back to an empty heart
+//Remove the .activated-heart class
+//Keep all your styling rules entirely in style.css. Do not manipulate any .style properties.
+//Only manipulate the DOM once the server request responds. Do not make the heart full until you're inside a successful .then block.*/
 
 
 //------------------------------------------------------------------------------
@@ -12,6 +80,7 @@ const FULL_HEART = '♥'
 //------------------------------------------------------------------------------
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+  console.log('clicked!')
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
